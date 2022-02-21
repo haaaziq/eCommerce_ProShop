d6420996bb22d04fc9227a6a6eb900e559ec9640
haaaziq <haziqkhan123@gmail.com>
2022-02-21 18:05:08 +0530

    Add product detail screen, currently fetching data from products.js file

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import {Row, Image, Col, Card, ListGroup, Button} from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+import products from '../products';
+
+// to get :id from url use => props.match.params.id OR useParams() hooks
+// Use it to access match.params
 
 const ProductScreen = () => {
-  return (
-    <div>Product</div>
+    const { id } = useParams();
+    const product = products.find((p) => p._id === id);
+  
+    return (
+    <>
+        <Link to={'/'}>
+            <Button variant='light'>Go Back</Button>
+        </Link>
+
+        <Row>
+            <Col md={6}>
+                <Image src={product.image} alt={product.name} fluid/>
+            </Col>
+
+            <Col md={3}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h3>{product.name}</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Rating value={product.rating} />{product.numReviews}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Price: ${product.price}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Description: {product.description}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>
+                                    Price:
+                                </Col>
+                                <Col>
+                                    <strong>${product.price}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>
+                                    Status:
+                                </Col>
+                                <Col>
+                                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item className='d-grid'>
+                            <Button type='button'>Add to Cart</Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
+    </>
   )
 }
 
-export default ProductScreen
+export default ProductScreen;
