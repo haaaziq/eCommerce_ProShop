4e705650b6a86ce235a4b26bbad5f6214dd301be
haaaziq <haziqkhan123@gmail.com>
2022-02-23 23:41:42 +0530

    Node-Express Backend setup, serving products

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// this is CommonJs... CommonJS => require, ES Modules => import
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API is running");
+});
+
+app.get("/api/products", (req, res) => {
+    res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
+
+app.listen(5000, console.log("Server started on port number 5000"));
