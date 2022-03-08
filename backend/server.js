c3e395d6582fe5260ee599b74d638b3be9e3c763
haaaziq <haziqkhan123@gmail.com>
2022-03-09 01:26:22 +0530

    add route not found middleware and error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from 'colors';
 // while importing file we have to include .js (modules are imported w/o .js)
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -13,12 +14,18 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+//======== ROUTES ===========
 app.get('/', (req, res) => {
     res.send(`API is running at port ${PORT}`);
 });
 
 // Product Routes
 app.use('/api/products', productRoutes);
+
+
+//======== MIDDLEWARE ===========
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(
     PORT, 
